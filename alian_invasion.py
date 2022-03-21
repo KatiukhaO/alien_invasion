c808eceef24cab22316012f9194878d7310c5a49
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-03-21 14:02:35 +0200

    Creat new module with class Settings which incapsulated all settings for game

--- alian_invasion.py
+++ alian_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """ Class for control resources and behavior of game """
@@ -9,8 +11,9 @@ class AlienInvasion:
     def __init__(self):
         """ Initialisation of game and creation game resources """
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -20,6 +23,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # For each run loop redrawing screen
+            self.screen.fill(self.settings.bg_color)
 
             # Show last drawing screen
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """ Class for saved all settings Alian Invasion game"""
+
+    def __init__(self):
+        """ Initialisation settings game """
+        # Screen parameters
+        self.screen_width = 1200
+        self.screen_height = 800
+        # Set background color
+        self.bg_color = (230, 230, 230)
